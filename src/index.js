bfc7ceb5b19e182ed3322283a531e68195ab9fe0
lee-vartha <lee-vartha@live.nmit.ac.nz>
2023-05-10 09:57:07 +1200

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 volumeOfContainer({ width: 2, length: 5, height: 1 })
+
+
+// function to figure out the volume of a container with width: 2, length: 5, height: 1
+
+function volumeOfContainer(sizes) {
+    let volume = 1;
+    for (let i = 0; i < sizes.length; i++) {
+        volume *= sizes[i];
+    }
+    return volume;
+}
+
+console.log(volumeOfContainer([2, 5, 1])); // 10
