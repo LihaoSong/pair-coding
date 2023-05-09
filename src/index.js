d2a013bba9c61e4e70aff5d2086f77d2e7c340f0
Lihao <Lihao-Song@live.nmit.ac.nz>
2023-05-10 06:46:03 +0800

    Hello World

--- src/index.js
+++ src/index.js
@@ -11,4 +11,33 @@ function volumeOfContainer({ width, length, height }) {
   }
   
   console.log(ConvertArray({a: 1, b: 2, c: 2}));
+
+  Numbers&Letters("Hello Wold 2023") - {"letters": 10, "numbers": 4}
+
+  function NumbersAndLetters(str) {
+    let letters = 0;
+    let numbers = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] >= "a" && str[i] <= "z" || str[i] >= "A" && str[i] <= "Z") {
+        letters++;
+      } else if (Number.isInteger(Number(str[i]))) {
+        numbers++;
+      }
+    }
   
+    return {
+      "letters": letters,
+      "numbers": numbers
+    };
+  }  
+  
+  console.log(NumbersAndLetters("Hello World 2023"));
+  
+//   Number.isNaN()
+//   .push
+//   .length
+//   .split()
+//   for loops
+//   if , else
+//   No to REGX
--- src/new.js
+++ src/new.js
@@ -1,14 +1,19 @@
-volumeOfContainer({ width: 2, length: 5, height: 1 })
-
-function volumeOfContainer({ width, length, height }) {
-    return width * length * height;
-  }
+function NumbersAndLetters(str) {
+    let letters = 0;
+    let numbers = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] >= "a" && str[i] <= "z" || str[i] >= "A" && str[i] <= "Z") {
+        letters++;
+      } else if (Number.isInteger(Number(str[i]))) {
+        numbers++;
+      }
+    }
   
-  console.log(volumeOfContainer({ width: 2, length: 5, height: 1 }));
+    return {
+      "letters": letters,
+      "numbers": numbers
+    };
+  }  
   
-  function ConvertArray(obj) {
-    return Object.entries(obj);
-  }
-  
-  console.log(ConvertArray({a: 1, b: 2, c: 2}));
-  
+  console.log(NumbersAndLetters("Hello World 2023"));
